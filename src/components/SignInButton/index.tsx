4f7eebd435b96683722de5dfe30e88106e4867a8
Lauro Costa <joaolauro94@gmail.com>
2021-04-15 00:32:37 -0300

    integration github auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
+
     >
       <FaGithub color="#04d261"/>
-      Lauro Costa
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={()=> signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
