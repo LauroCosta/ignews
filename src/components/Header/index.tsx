efe3c884f9ddf8c11c3ac31166d99d7dfc48442d
Lauro Costa <joaolauro94@gmail.com>
2021-05-22 15:40:27 -0300

    header navigation included

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,29 @@
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news"/>
+        <ActiveLink activeClassName={styles.active} href="/">
+          <a><img src="/images/logo.svg" alt="ig.news"/></a>
+        </ActiveLink>
+        
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+        
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>  
+          </ActiveLink>
+
         </nav>
 
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
+import {format, parseISO} from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
@@ -50,22 +52,20 @@ export const getStaticProps: GetStaticProps = async () => {
   );
 
   const posts = response.results.map((post) => {
+
+
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
       excerpt:
         post.data.content.find((content) => content.type === "paragraph")
           ?.text ?? "",
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-        "pt-BR",
-        {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }
-      ),
+      updatedAt: format(parseISO(post.last_publication_date), "dd 'de' MMMM 'de' yyyy", {
+        locale: ptBR
+      })
     };
   });
+
 
   return {
     props: {
