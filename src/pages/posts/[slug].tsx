6af9e33cf2ba8ae47b4a54fcae0b350aab818f13
Lauro Costa <joaolauro94@gmail.com>
2021-05-23 01:19:25 -0300

    signature validation

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,19 +37,21 @@ export default function Post({ post }: PostProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  params,
-}) => {
-  const session = await getSession({ req });
+export const getServerSideProps: GetServerSideProps = async ({ req, params,}) => {
+  const session = await getSession({req});
   const { slug } = params;
-  // if (!session) {
 
-  // }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID("post", String(slug), {});
-  console.log(response);
   const post = {
     slug,
     title: RichText.asText(response.data.title),
